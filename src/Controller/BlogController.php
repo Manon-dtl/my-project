2704ce7bccb0f03f662a7aae05285e083cff9744
Manon-dtl <manon.dauteuilco@gmail.com>
2020-11-23 10:44:39 +0100

    Form commentaire

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,6 +11,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\ArticleType;
+use App\Entity\Comment;
+use App\Form\CommentType;
+
 
 
 class BlogController extends AbstractController
@@ -72,10 +75,26 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show (Article $article){
+    public function show (Article $article, Request $request, EntityManagerInterface $manager){
+
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setCreatedAt(new \DateTime())
+                    ->setArticle($article);
+
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id'=> $article->getId()
+            ]);
+        }
 
         return $this->render('blog/show.html.twig', [
             'article' => $article,
+            'commentForm' => $form->createView()
         
         ]);
     }
